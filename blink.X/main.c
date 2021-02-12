164a851fa6296bef3fb3ebc4c2807600a7b65225
21192694 <21192694@DSN1271158611.cfp127aluno.local>
2021-02-12 16:52:58 -0200

    Configurei e implementei ao blink no pino RD7.

--- blink.X/main.c
+++ blink.X/main.c
@@ -1,13 +1,23 @@
 /*
  * File:   main.c
- * Author: 21192694
+ * Author: Gabriel Ruanes Melquiades 
  *
  * Created on 12 de Fevereiro de 2021, 16:01
  */
 
 #include <xc.h>
+#include "config.h"
 
 void main(void) 
 {
-    return;
+        TRISDbits.TRISD7 = 0;
+        PORTDbits.RD7 = 0;
+        
+    while ( 1 )
+    {
+        PORTDbits.RD7 = 1;
+        __delay_ms(500);
+        PORTDbits.RD7 = 0;
+        __delay_ms(500);
+    }
 }
